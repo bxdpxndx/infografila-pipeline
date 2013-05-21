74f4cca57cbb8b07443a5c30201e8cbcba47ba1a
kiarot <kiarot@2c1ef683-5c96-4453-838b-7e99e844e527>
2013-05-21 16:40:23 +0000

    leer fichero, vector vertices y vector de poligonos
    git-svn-id: https://subversion.assembla.com/svn/infografia-pipeline@6 2c1ef683-5c96-4453-838b-7e99e844e527

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,61 @@ int main()
 	// nº vertices espacio nº poligonos (primera linea)
 	//coordenadas a espacio b espacio c (segunda en adelante) 
 	int vertices;
-	int polignos;
-	float a, b, c; //a b c son las coordenadas
+	int poligonos;
+	float a, b, c, d, e; //a b c son las coordenadas
 	
-	coordenadas xyz; //vector de coordenadas xyz
-	
-        if( !fe )
-        { std::cout << "-!- error -!-" << std::endl; } 
+	coordenadas xyz; //vector de vertices xyz
+	coordenadas pols; //vector de coordenadas xyz
 
-	fe >> vertices >> polignos;
+        if( !fe ) { std::cout << "-!- error -!-" << std::endl; } 
+
+	fe >> vertices >> poligonos;
 	
 	std::cout << "vertices polignos" << std::endl;	
-	std::cout << vertices << "        " << polignos<< std::endl;
+	std::cout << vertices << "        " << poligonos<< std::endl;
 	std::cout << "coodenada" << std::endl;
+	std::cout << "fichero original " << std::endl;
 
-	std::cout << "a b c" << std::endl;
+int bux=vertices, cux=poligonos;
 
-	while( !fe.eof() ) {
-		fe >> a >> b >>c;
-		std::cout << a <<" "<< b << " " << c << std::endl;	
-		xyz.push_back(a);
-		xyz.push_back(b);
-		xyz.push_back(c);
+	while( !fe.eof() ) 
+	{
+		if (bux>0){		
+			fe >> a >> b >>c;
+			xyz.push_back(a);
+			xyz.push_back(b);
+			xyz.push_back(c);
+			bux--;
+		}
+		if (cux>0 && bux ==0 ){//si bux=vertices es 0 ya ha acabado de leer los vertices.	
+		//fe >> a >> b >>c>>d;
+		fe>>a;
+	
+			if ( a == 4)
+			{
+				fe >> b >> c >> d >> e;
+				pols.push_back(a);
+				pols.push_back(b);
+				pols.push_back(c);
+				pols.push_back(d);
+				pols.push_back(e);
+				std::cout<<a<<b<<c<<d<<e<<std::endl;
+			}
+			if (a == 3)	
+			{
+				fe >> b >> c >> d;
+				pols.push_back(a);
+				pols.push_back(b);
+				pols.push_back(c);
+				pols.push_back(d);				std::cout<<a<<b<<c<<d<<std::endl;
+			}
+		}
 	}
 
-	std::cout << "comprobación vector de coordenadas" << std::endl;
+	std::cout << "comprobación vector de vertices" << std::endl;
+
 	int aux=0; //solo sirve para mostrar linias de 3, solo decorativo
+
 	for (std::vector<float>::iterator it=xyz.begin() ; it!=xyz.end() ; ++it) {
 		std::cout << *it<<" ";
 		if (aux<2) aux++;
@@ -45,6 +74,16 @@ int main()
 			aux=0; 
 			std::cout<< std::endl;
 			}	
+	}
+	std::cout << "comprobación vector de poligonos" << std::endl;
+
+	aux=0;
+	for (std::vector<float>::iterator it=pols.begin() ; it!=pols.end() ; ++it) {
+		std::cout << *it<<" ";
+		if (aux==0){ aux=*it+1; }
+		if(aux>0) { aux--; }
+		if (aux==0){ aux==0;	std::cout << std::endl; }	
+			
 	}
 	fe.close(); //cerrar fichero 
 }
